33b733faad8bccefe12ac22ad6b98c8439bfe1d9
jylmarcus <maplecow@gmail.com>
2023-04-17 19:25:40 +0800

    added main loop and bugfixes

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,21 +1,24 @@
 package sg.edu.nus.iss;
 
-import java.io.console;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class App 
 {
     //list method
     public static void list(ArrayList<String> shoppingCart){
-        for (int i = 0; i< shoppingCart.length(); i++) {
+        if(shoppingCart.size() == 0){
+            System.out.println("Your cart is empty");
+        }
+        for (int i = 0; i< shoppingCart.size(); i++) {
             System.out.println((i+1) + ". " + shoppingCart.get(i));
         }
     }
     //add method
-    public static void add(String[] args) {
+    public static void add(String[] args, ArrayList<String> shoppingCart) {
         for (String item:args) {
             if(shoppingCart.contains(item.trim().toLowerCase())){
-                System.out.println("You have " + item.trim().toLowerCase() " in your cart");
+                System.out.println("You have " + item.trim().toLowerCase() + " in your cart");
                 break;
             }
             shoppingCart.add(item.trim().toLowerCase());
@@ -23,12 +26,10 @@ public class App
         }
     }
     //delete method
-    public static void delete(Integer[] args) {
-        if(shoppingCart.contains(args[0])){
-            System.out.println(shoppingCart.get(args[0]) " removed from cart");
-        }
-        if(shoppingCart.remove(args[0])){
-            shoppingCart.remove(args[0]);
+    public static void delete(Integer index, ArrayList<String> shoppingCart) {
+        if(index >=0 && index < shoppingCart.size()){
+            System.out.println(shoppingCart.get(index) + " removed from cart");
+            shoppingCart.remove(shoppingCart.get(index));
         } else {
             System.out.println("Incorrect item index");
         }
@@ -36,17 +37,34 @@ public class App
 
     public static void main( String[] args )
     {
-        Boolean runCart = true;
         ArrayList<String> shoppingCart = new ArrayList<String>();
-        Console cons = System.console();
         System.out.println("Welcome to your shopping cart");
-        String command = ""
-        String input = ""
-
-        while (runCart) {
-            input = cons.readLine();
-            command = input.split("\\s+")[0];
-            switch
+        String command = "";
+        String input = "";
+        
+        //loop until user quits app
+        while (!command.equals("quit")) {
+            Scanner scan = new Scanner(System.in);
+            command = scan.next();
+            input = scan.nextLine();
+            switch(command) {
+                case "list":
+                    list(shoppingCart);
+                    break;
+                case "add":
+                    String[] addItems = input.split(",");
+                    add(addItems, shoppingCart);
+                    break;
+                case "delete":
+                    Integer delItemIndex = Integer.parseInt(input.trim())-1;
+                    delete(delItemIndex, shoppingCart);
+                    break;
+                case "quit":
+                    break;
+                default:
+                    System.out.println("Please enter list, add, delete or quit");
+                    break;
+            }
         }
 
 
