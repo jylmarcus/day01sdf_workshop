71b260915afc244bd29129a29b5f574d10d6619e
jylmarcus <maplecow@gmail.com>
2023-04-18 14:45:47 +0800

    Added alternative code

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -35,10 +35,24 @@ public class App
         }
     }
 
+    public static Boolean isNumeric(String string){
+        if(string == null){
+            return false;
+        }
+
+        try {
+            int i = Integer.parseInt(string);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
     public static void main( String[] args )
     {
         ArrayList<String> shoppingCart = new ArrayList<String>();
         System.out.println("Welcome to your shopping cart");
+        System.out.println("Type 'help to show a list of commands");
         String command = "";
         String input = "";
         
@@ -48,24 +62,83 @@ public class App
             command = scan.next();
             input = scan.nextLine();
             switch(command) {
+                case "help":
+                    System.out.println("'list' to show a list of items in the shopping cart");
+                    System.out.println("'add' <item name>, <item name>...");
+                    System.out.println("'delete' <item number>");
+                    System.out.println("'quit' to exit the program");
                 case "list":
                     list(shoppingCart);
                     break;
                 case "add":
-                    String[] addItems = input.split(",");
+                    //replace commas with whitespace
+                    input = input.replace(",", " ");
+                    String[] addItems = input.split(" ");
                     add(addItems, shoppingCart);
                     break;
                 case "delete":
-                    Integer delItemIndex = Integer.parseInt(input.trim())-1;
-                    delete(delItemIndex, shoppingCart);
+                    if(isNumeric(input)){
+                        Integer delItemIndex = Integer.parseInt(input.trim())-1;
+                        delete(delItemIndex, shoppingCart);
+                    }
                     break;
                 case "quit":
                     break;
                 default:
-                    System.out.println("Please enter list, add, delete or quit");
+                    System.out.println("Please enter list, add, delete, quit or help");
                     break;
             }
         }
+
+        /*Alternative method using console
+         * Console con = System.console();
+         * String input = "";
+         * 
+         * while(!input.equals("quit")){
+         *  input = con.readLine("Type 'help to show list of commands >>");
+         * 
+         *  if(input.equals("help")){
+         *      do something
+         *  }
+         *  
+         *  if(input.startsWith("add")){
+         *      input = input.replace(","," ");
+         *      Scanner scan = new Scanner(input.substring(beginIndex:4));
+         *      
+         *      
+         *      String content = "";
+         *      while(scan.hasNext()){
+         *          content = scan.next();
+         *  
+         *          cartItems.add(content);
+         *      }
+         *  }
+         * 
+         *  if(input.equals("list")){
+         *      int i = 0;
+         *      for(String item: cartItems) {
+         *          i++;
+         *          System.out.println(i + ". " + item);
+         *      }
+         *  }
+         *  
+         *  if(input.startsWith("delete")){
+         *      Scanner scan = new Scanner(input.substring(6));
+         * 
+         *      String content = "";
+         *      while(scan.hasNext()){
+         *          content = scan.next();
+         *          int listIndex = Integer.parseInt(content);
+         * 
+         *          if(listIndex < cartItems.size()){
+         *              cartItems.remove(listIndex);
+         *          } else {
+         *              System.err.println("Incorrect item index");
+         *          }
+         *      }
+         *  }
+         * }
+         */
 
 
     }
