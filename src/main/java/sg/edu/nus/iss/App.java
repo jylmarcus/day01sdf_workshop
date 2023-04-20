980fcb826cba433ee61ddc36c2ba9593489e5224
jylmarcus <maplecow@gmail.com>
2023-04-20 10:04:38 +0800

    Added ShoppingCartDB class and methods

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,24 +1,24 @@
 package sg.edu.nus.iss;
 
 import java.util.Scanner;
-import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class App 
-{
-    //list method
-    public static void list(ArrayList<String> shoppingCart){
-        if(shoppingCart.size() == 0){
+public class App {
+    // list method
+    public static void list(ArrayList<String> shoppingCart) {
+        if (shoppingCart.size() == 0) {
             System.out.println("Your cart is empty");
         }
-        for (int i = 0; i< shoppingCart.size(); i++) {
-            System.out.println((i+1) + ". " + shoppingCart.get(i));
+        for (int i = 0; i < shoppingCart.size(); i++) {
+            System.out.println((i + 1) + ". " + shoppingCart.get(i));
         }
     }
-    //add method
+
+    // add method
     public static void add(String[] args, ArrayList<String> shoppingCart) {
-        for (String item:args) {
-            if(shoppingCart.contains(item.trim().toLowerCase())){
+        for (String item : args) {
+            if (shoppingCart.contains(item.trim().toLowerCase())) {
                 System.out.println("You have " + item.trim().toLowerCase() + " in your cart");
                 break;
             }
@@ -26,9 +26,10 @@ public class App
             System.out.println(item.trim().toLowerCase() + " added to cart");
         }
     }
-    //delete method
+
+    // delete method
     public static void delete(Integer index, ArrayList<String> shoppingCart) {
-        if(index >=0 && index < shoppingCart.size()){
+        if (index >= 0 && index < shoppingCart.size()) {
             System.out.println(shoppingCart.get(index) + " removed from cart");
             shoppingCart.remove(shoppingCart.get(index));
         } else {
@@ -36,8 +37,8 @@ public class App
         }
     }
 
-    public static Boolean isNumeric(String string){
-        if(string == null){
+    public static Boolean isNumeric(String string) {
+        if (string == null) {
             return false;
         }
 
@@ -49,113 +50,152 @@ public class App
         return true;
     }
 
-    public static void main( String[] args )
-    {
-        //specify saved cart directory
-        File cartdb = new File(args[0]);
-        if (!cartdb.exists()) {
-            cartdb.mkdir();
-        }
+    public static void main(String[] args) throws IOException {
+        // specify saved cart directory
+        ShoppingCartDB scdb = new ShoppingCartDB(args[0]);
 
         ArrayList<String> shoppingCart = new ArrayList<String>();
         System.out.println("Welcome to your shopping cart");
-        System.out.println("Type 'help to show a list of commands");
+        System.out.println("Type 'login <username>' to begin");
         String command = "";
         String input = "";
-        
-        //loop until user quits app
+        String currentUser = "";
+
+        // loop until user quits app
         while (!command.equals("quit")) {
             Scanner scan = new Scanner(System.in);
-            command = scan.next();
+            command = scan.next().trim();
             input = scan.nextLine();
-            switch(command) {
-                case "help":
-                    System.out.println("'login <username> to load or create a cart");
-                    System.out.println("'list' to show a list of items in the shopping cart");
-                    System.out.println("'add' <item name>, <item name>...");
-                    System.out.println("'delete' <item number>");
-                    System.out.println("'save' to save current cart");
-                    System.out.println("'users' to show list of users");
-                    System.out.println("'quit' to exit the program");
-                case "login":
-                    break;
-                case "list":
-                    list(shoppingCart);
-                    break;
-                case "add":
-                    //replace commas with whitespace
-                    input = input.replace(",", " ");
-                    String[] addItems = input.split(" ");
-                    add(addItems, shoppingCart);
-                    break;
-                case "delete":
-                    if(isNumeric(input)){
-                        Integer delItemIndex = Integer.parseInt(input.trim())-1;
-                        delete(delItemIndex, shoppingCart);
-                    }
-                    break;
-                case "save":
-                    break;
-                case "users":
-                    break;
-                case "quit":
-                    break;
-                default:
-                    System.out.println("Please enter list, add, delete, quit or help");
-                    break;
+            if ((currentUser == "") && (!command.equals("login"))) {
+                System.out.println("Please login before taking any actions");
+            } else {
+                switch (command) {
+
+                    case "help":
+                        System.out.println("'login' <username> to load or create a cart");
+                        System.out.println("'list' to show a list of items in the shopping cart");
+                        System.out.println("'add' <item name>, <item name>...");
+                        System.out.println("'delete' <item number>");
+                        System.out.println("'save' to save current cart");
+                        System.out.println("'users' to show list of users");
+                        System.out.println("'quit' to exit the program");
+                        break;
+
+                    case "login":
+                        // pass input to shoppingcartdb login
+                        if (input.length() == 0) {
+                            System.out.println("Please enter a username");
+                            break;
+                        }
+                        if (currentUser == "") {
+                            scdb.login(input.trim());
+                        } else {
+                            shoppingCart.clear();
+                            shoppingCart.addAll(scdb.login(input.trim()));
+                        }
+                        // set currentuser to input
+                        currentUser = input.trim();
+                        // print login message and empty/items in cart message
+                        if (shoppingCart.size() > 0) {
+                            System.out.println(currentUser + ", your cart contains the following items");
+                            // print list of items if cart contains items
+                            list(shoppingCart);
+                        } else {
+                            System.out.println(currentUser + ", your cart is empty");
+                        }
+
+                        break;
+
+                    case "list":
+                        list(shoppingCart);
+                        break;
+
+                    case "add":
+                        // replace commas with whitespace
+                        input = input.replace(",", "");
+                        String[] addItems = input.trim().split(" ");
+                        add(addItems, shoppingCart);
+                        break;
+                    case "delete":
+                        if (isNumeric(input)) {
+                            Integer delItemIndex = Integer.parseInt(input.trim()) - 1;
+                            delete(delItemIndex, shoppingCart);
+                        }
+                        break;
+
+                    case "save":
+                        // pass currentuser and shoppingcart to scdb save
+                        scdb.save(currentUser, shoppingCart);
+                        // print cart saved
+                        System.out.println("Your cart has been saved");
+                        break;
+
+                    case "users":
+                        // call users method from scdb
+                        scdb.users();
+                        break;
+
+                    case "quit":
+                        scan.close();
+                        break;
+
+                    default:
+                        System.out.println("Please enter list, add, delete, quit or help");
+                        break;
+                }
             }
         }
 
-        /*Alternative method using console
+        /*
+         * Alternative method using console
          * Console con = System.console();
          * String input = "";
          * 
          * while(!input.equals("quit")){
-         *  input = con.readLine("Type 'help to show list of commands >>");
+         * input = con.readLine("Type 'help to show list of commands >>");
          * 
-         *  if(input.equals("help")){
-         *      do something
-         *  }
-         *  
-         *  if(input.startsWith("add")){
-         *      input = input.replace(","," ");
-         *      Scanner scan = new Scanner(input.substring(beginIndex:4));
-         *      
-         *      
-         *      String content = "";
-         *      while(scan.hasNext()){
-         *          content = scan.next();
-         *  
-         *          cartItems.add(content);
-         *      }
-         *  }
+         * if(input.equals("help")){
+         * do something
+         * }
          * 
-         *  if(input.equals("list")){
-         *      int i = 0;
-         *      for(String item: cartItems) {
-         *          i++;
-         *          System.out.println(i + ". " + item);
-         *      }
-         *  }
-         *  
-         *  if(input.startsWith("delete")){
-         *      Scanner scan = new Scanner(input.substring(6));
+         * if(input.startsWith("add")){
+         * input = input.replace(","," ");
+         * Scanner scan = new Scanner(input.substring(beginIndex:4));
          * 
-         *      String content = "";
-         *      while(scan.hasNext()){
-         *          content = scan.next();
-         *          int listIndex = Integer.parseInt(content);
          * 
-         *          if(listIndex < cartItems.size()){
-         *              cartItems.remove(listIndex);
-         *          } else {
-         *              System.err.println("Incorrect item index");
-         *          }
-         *      }
-         *  }
+         * String content = "";
+         * while(scan.hasNext()){
+         * content = scan.next();
+         * 
+         * cartItems.add(content);
+         * }
+         * }
+         * 
+         * if(input.equals("list")){
+         * int i = 0;
+         * for(String item: cartItems) {
+         * i++;
+         * System.out.println(i + ". " + item);
+         * }
+         * }
+         * 
+         * if(input.startsWith("delete")){
+         * Scanner scan = new Scanner(input.substring(6));
+         * 
+         * String content = "";
+         * while(scan.hasNext()){
+         * content = scan.next();
+         * int listIndex = Integer.parseInt(content);
+         * 
+         * if(listIndex < cartItems.size()){
+         * cartItems.remove(listIndex);
+         * } else {
+         * System.err.println("Incorrect item index");
+         * }
+         * }
+         * }
          * }
          */
-
 
     }
 }
--- src/main/java/sg/edu/nus/iss/ShoppingCartDB.java
+++ src/main/java/sg/edu/nus/iss/ShoppingCartDB.java
@@ -1,5 +1,90 @@
 package sg.edu.nus.iss;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class ShoppingCartDB {
-    
+    private String dirPath;
+    private File directory;
+
+    public ShoppingCartDB() {
+
+    }
+
+    public ShoppingCartDB(String dirPath) {
+        this.dirPath = dirPath;
+        this.directory = new File(dirPath);
+        //create directory if it does not exist upon initialization
+        if (!directory.exists()) {
+            directory.mkdir();
+        } else {
+            System.out.println("Directory already exists");
+        }
+    }
+
+    private String mkFilePath(String fileName) {
+        return new String(dirPath + File.separator + fileName);
+    }
+
+    public void writeFile(String fileName, ArrayList<String> shoppingCart) throws IOException {
+        FileWriter fw = new FileWriter(new File(mkFilePath(fileName)));
+        for(String cartItem: shoppingCart) {
+            fw.append(cartItem);
+            fw.append("\n");
+        }
+        fw.flush();
+        fw.close();
+    }
+
+    public ArrayList<String> login(String fileName) throws IOException {
+        String user = mkFilePath(fileName);
+        File userfile = new File(user);
+        // if filename exist
+
+        if (userfile.exists()) {
+            // read file
+            // output to and return shopping cart array list
+
+            BufferedReader br = new BufferedReader(new FileReader(user));
+            String line;
+            ArrayList<String> loadCart = new ArrayList<>();
+
+            while((line = br.readLine()) != null) {
+                loadCart.add(line);
+            }
+
+            br.close();
+            return loadCart;
+        } else {
+            userfile.createNewFile();
+            return new ArrayList<String>();
+            // create file with filename
+            // return empty shoppingcart but do not assign it using if else statement
+        }
+        // also need a variable to store current fileName somewhere for later use
+    }
+
+    public void save(String fileName, ArrayList<String> shoppingCart) throws IOException {
+
+        if(fileName == "") {
+            System.out.println("Please login before saving.");
+        } else {
+            writeFile(fileName, shoppingCart);
+        }
+        
+    }
+
+    public void users() {
+        // use listfiles method on dirpath File
+        File[] users = directory.listFiles();
+        // use for loop with index due to formatting to print list of users
+        if(users.length > 0){
+            for(int i = 0; i < users.length; i++) {
+                System.out.println((i+1) + ". " + users[i].getName());
+            }
+        } else {
+            System.out.println("There are no users!");
+        }
+    }
+
 }
